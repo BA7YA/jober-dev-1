8a4c6c1645fae979111ebcc56a0731edb202aded
BA7YA <iu.starodubow@yandex.ru>
2021-07-27 16:03:54 +0500

    Header added, main block optimized

--- resources/js/App/App.js
+++ resources/js/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route,} from 'react-router-dom';
 import Header from '../Main components/Header/View/Header';
+import SideMenu from '../Main components/SideMenu/View/SideMenu';
 import Home from '../Pages/Home/View/Home';
 import './App.scss';
 
@@ -13,6 +14,7 @@ function App() {
                         <Header />
                         </div>
                     <div className="body-block">
+                        <SideMenu />
                     <Route exact path="/">
                         <Home />
                     </Route>
