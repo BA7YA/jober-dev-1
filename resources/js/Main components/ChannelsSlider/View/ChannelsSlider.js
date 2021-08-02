2d7a14f0bee37b60071131d40e319ec18014912c
BA7YA <iu.starodubow@yandex.ru>
2021-08-02 15:14:13 +0500

    Changed Structure and optimize structure

---
+++ resources/js/Main components/ChannelsSlider/View/ChannelsSlider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChannelsSlider() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChannelsSlider
---
+++ resources/js/Main components/UserSideBlock/View/UserSideBlock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserSideBlock.scss';
+
+function UserSideBlock() {
+    return (
+        <div className="userside-block">
+            
+        </div>
+    )
+}
+
+export default UserSideBlock
